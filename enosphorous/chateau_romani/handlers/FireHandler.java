8e546e23ee56f7f152cc15cd244150f2f9520fbf
Enosphorous <enosphorous@gmail.com>
2013-09-12 15:20:18 -0700

    Implemented FireHandler, thanks to SynthTones.

--- enosphorous/chateau_romani/handlers/FireHandler.java
+++ enosphorous/chateau_romani/handlers/FireHandler.java
@@ -1,8 +1,26 @@
 package enosphorous.chateau_romani.handlers;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import enosphorous.chateau_romani.common.Items;
+
 public class FireHandler
 {
-/**
- * PlayerInteractEvent method
- */
+    @ForgeSubscribe
+    public void onPlayerInteract(PlayerInteractEvent event)
+    {
+EntityPlayer player = event.entityPlayer;
+InventoryPlayer inv = player.inventory;
+if(player.worldObj.getBlockId(event.x, event.y + 1, event.z) == Block.fire.blockID && inv.getCurrentItem() !=null && inv.getCurrentItem().itemID == Item.glassBottle.itemID)
+{
+	player.inventory.setInventorySlotContents(player.inventory.currentItem, new ItemStack(Items.bottled_fire));
+	player.worldObj.setBlockToAir(event.x, event.y + 1, event.z);
+}
+    }
 }
